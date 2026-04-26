ba942e0cd308a13ad7d7cafdf2df45a5c08697f9
mkxdesu <mkxdesu@gmail.com>
2026-04-26 11:55:17 +0700

    Added handling for out of range IDs

--- book-service/bootstrap/app.php
+++ book-service/bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (ModelNotFoundException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Book not found.'
+                ], 404);
+            }
+        });
     })->create();
