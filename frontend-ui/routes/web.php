900ca318de5b0ec7a34e9b4c5b919f702377105d
Shinta Devi Arsy Zelika <shintadaz@gmail.com>
2026-05-12 10:40:38 +0700

    Simpan progres fitur buku UTS

--- frontend-ui/routes/web.php
+++ frontend-ui/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use PHPUnit\Metadata\Api\Dependencies;
 
 // Halaman Katalog Buku (Utama)
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
@@ -18,3 +19,13 @@ Route::post('/pinjam/{id}', [DashboardController::class, 'store'])->name('book.b
 // Jalur untuk aksi pengembalian buku
 Route::put('/transaksi/{id}/return', [App\Http\Controllers\DashboardController::class, 'returnBook'])->name('transaksi.return');
 
+// instal
+// Composer require pestphp/pest --dev --with-all-Dependencies
+
+// intall pest plugin laravel
+// composer require pestphp/pest-plugin-laravel --dev --with-all-Dependencies
+
+// jalankan vendor
+// ./vendor/bin/pest --init
+// ./vendor/bin/pest --version
+// htttp.cat
